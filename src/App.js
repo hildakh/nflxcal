f69f2aa2daa69c2a748f45e831472da67f0507f0
drystar <chris.drysdale12@gmail.com>
2020-08-25 22:41:21 -0700

    create GET request for JSON data

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import axios from "axios";
 // import logo from "./logo.svg";
 
 moment.locale("en-GB");
@@ -12,7 +13,22 @@ const myEventsList = {};
 
 class App extends Component {
   constructor() {}
-  componentDidMount() {}
+  componentDidMount() {
+    let self = this;
+    axios
+      .get("https://api.jsonbin.io/b/5f45f420514ec5112d0e794a")
+      .then(function (response) {
+        console.log(response.data);
+        let releases = response.data;
+
+        for (let i = 0; i < releases.length; i++) {
+          console.log(releases[i]);
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
   render() {
     return (
       <Calendar
