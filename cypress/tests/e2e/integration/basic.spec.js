41c9fe123e3fa3c07d74466cdc3705121a85ed39
drystar <chris.drysdale12@gmail.com>
2020-08-26 00:40:55 -0700

    create basic navigartion tests

--- cypress/tests/e2e/integration/basic.spec.js
+++ cypress/tests/e2e/integration/basic.spec.js
@@ -1,5 +1,13 @@
-describe("basic workflow", () => {
+describe("basic", () => {
   it("renders without crashing", () => {
     cy.visit("/");
+  });
+
+  it("should navigate to August", () => {
+    cy.visit("/");
+
+    cy.contains("[data-testid=month]", "August")
+      .click()
+      .should("have.class", "month--selected");
   });
 });
